3f24cf8d5fc160aa7f349cfa3fcb5af218883810
falves <fabiano.alves@adp.com>
2023-10-31 09:03:15 -0300

    chore: Conexao com DB

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'senhas'
+mysql = MySQL(app)
+
 @app.route("/painel")
 def painel():
-    senha = 100
-    return render_template('painel.html', senha=senha)
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT senhas.senha FROM senhas")
+    data = cursor.fetchone()
+    senha = data
+
+    cursor.execute("SELECT senhas_chamadas.senha FROM senhas.senhas_chamadas;")
+    chamadas = cursor.fetchall()
+    cursor.close()
+    return render_template('painel.html', senha=senha, chamadas=chamadas)
 
 @app.route("/gerar")
 def gera_senha():
